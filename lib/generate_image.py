0e92d6d63497947f0da3183854f8d4587dfae67a
Tingting Jiang <virjtt03@vt.edu>
2014-12-08 11:40:33 -0500

    Save the generated figure(s) to image file(s).

--- lib/generate_image.py
+++ lib/generate_image.py
@@ -1,14 +1,16 @@
 import h5py
 import numpy
 import matplotlib.pyplot as plt
-from os import path
+import os
 import time
+# regular expression
+import re
 
 # read HDF5 file and raise exceptions
 def readH5File(fileName):
 	if not(fileName.endswith('.h5')): 
 		raise NameError('File name must end with .h5')
-	elif not(path.exists(fileName)):
+	elif not(os.path.exists(fileName)):
 		raise ValueError("No such file: {} exisits.".format(fileName))
 	else:
 		f = h5py.File(fileName, 'r')
@@ -21,38 +23,54 @@ def readH5File(fileName):
 def get_dataset(h5_file, key):
 	return h5_file[key]
 
+# save figure to image file
+# fileName: h5 file
+# fig: the Figure object
+def saveToFile(fileName, fig):
+	# image directory
+	imgDir = 'images'
+	if not os.path.exists(imgDir):
+		os.makedirs(imgDir)
+
+	pattern = re.compile(r'\.h5$')
+	baseName = pattern.sub('.png', os.path.basename(fileName))
+	imgFileName = os.path.join(os.path.abspath(imgDir), baseName)
+	plt.savefig(imgFileName)
+	print('Saved the generated figure of {} to {}!'.format(fileName, imgFileName))
+
 # 2D plot the data in the h5 file with x-axis as time in milliseconds)
 # and y-axis as data unit in acceleration, g 
-# p_f: parent file or group
-def generate_image(p_f):
-	v_label = p_f['/'].attrs.get('units')
-	tic = time.clock()
-	# get one of the dataset
-	for key in p_f.keys():
-		dataset = get_dataset(p_f, key)
-		sample_tuple = dataset[0, :]
-		sample_size = sample_tuple.size
-		
-		print("dataset size:", sample_size, "max:", max(sample_tuple), "min:", min(sample_tuple), "average:", numpy.mean(sample_tuple), "median:", numpy.median(sample_tuple))
-
-		x = numpy.arange(0, 900000, 900000/sample_size)
-		y = sample_tuple
-
-		plt.plot(x, y)
-		plt.xlabel('millisecond)')
-		plt.ylabel(v_label.decode("utf-8"))
-		plt.title('test_figure')
-		plt.show()
-	toc = time.clock()
-	print("Process time:", toc-tic)
-
-def main():
-	fileName = "data_channel_1_02-Sep-2014-1330.h5"
+# fileName: the absoulte path to h5 file
+def generate_image(fileName):
 	try:
 		f = readH5File(fileName)
-		generate_image(f)
+		v_label = f['/'].attrs.get('units')
+		tic = time.clock()
+		# get one of the dataset
+		for key in f.keys():
+			dataset = get_dataset(f, key)
+			sample_tuple = dataset[0, :]
+			sample_size = sample_tuple.size
 		
-		print("The data from this HDF file has been successfully plotted as a figure!")
+			print("dataset size:", sample_size,
+					"max:", max(sample_tuple), 
+					"min:", min(sample_tuple), 
+					"average:", numpy.mean(sample_tuple), 
+					"median:", numpy.median(sample_tuple))
+
+			x = numpy.arange(0, 900000, 900000/sample_size)
+			y = sample_tuple
+
+			plt.plot(x, y)
+			plt.xlabel('millisecond')
+			plt.ylabel(v_label.decode("utf-8"))
+			plt.title('test_figure')
+			#plt.show()
+			fig = plt.gcf()
+			saveToFile(fileName, fig)
+		toc = time.clock()
+		print("Total processing time:", toc-tic)
+		
 		f.close()
 	except NameError as e:
 		print('Bad file name:', e)
@@ -60,6 +78,13 @@ def main():
 		print(e)
 	except IOError as e:
 		print('Cannot read file:', e)
+	
+
+def main():
+	fileName = "data_channel_1_02-Sep-2014-1330.h5"
+
+	generate_image(fileName)
+		
 
 # main() gets called at the end of the file
 if __name__ == "__main__": main()
